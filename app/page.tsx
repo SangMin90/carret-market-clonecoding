d2c9b099e04bb9c9357b5d57a422fa7f3eddabe5
smLee <sm.lee@care-biz.co.kr>
2024-05-09 00:34:03 +0900

    Home 화면 HTML 작성

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,39 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <main
-      className="bg-gray-300 h-screen flex
-        items-center justify-center p-5"
+    <div
+      className="flex flex-col items-center
+      justify-between min-h-screen p-6"
     >
       <div
-        className="bg-white shadow-lg p-5
-      rounded-3xl w-full max-w-screen-sm flex
-      flex-col gap-4"
+        className="my-auto flex flex-col
+        items-center gap-2 *:font-medium"
       >
-        <input />
-        <a href="#">aaaa</a>
-        <button className="btn">Submit</button>
+        <span className="text-9xl">🥕</span>
+        <h1 className="text-4xl">당근</h1>
+        <h2 className="text-2xl">당근 마켓에 어서오세요!</h2>
       </div>
-    </main>
+      <div
+        className="flex flex-col items-center
+        gap-3 w-full"
+      >
+        <Link
+          href="/create-account"
+          className="w-full bg-orange-500
+          text-lg font-medium py-2.5 rounded-md
+          text-center hover:bg-orange-400
+          transition-colors text-white"
+        >
+          시작하기
+        </Link>
+        <div className="flex gap-2">
+          <span>이미 게정이 있나요?</span>
+          <Link href="/login" className="hover:underline">
+            로그인
+          </Link>
+        </div>
+      </div>
+    </div>
   );
 }
