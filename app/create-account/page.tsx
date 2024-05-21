842dd059c4c4a7911aa234c64ff72830b06f210e
smLee <sm.lee@care-biz.co.kr>
2024-05-21 23:55:39 +0900

    ZOD Form 유효성 검사

--- app/create-account/page.tsx
+++ app/create-account/page.tsx
@@ -1,10 +1,15 @@
+"use client";
+
 import {} from "@heroicons/react/16/solid";
 import {} from "@heroicons/react/24/outline";
 import FormInput from "../../components/form-input";
 import FormButton from "../../components/form-button";
 import SocialLogin from "../../components/social-login";
+import { useFormState } from "react-dom";
+import { createAccount } from "./action";
 
 export default function CreateAccount() {
+  const [state, dispatch] = useFormState(createAccount, null);
   return (
     <div
       className="flex flex-col gap-10
@@ -17,24 +22,38 @@ export default function CreateAccount() {
         <h1 className="text-2xl">안녕하세요!</h1>
         <h2 className="text-xl">Fill in the form below to join!</h2>
       </div>
-      <form className="flex flex-col gap-3">
+      <form action={dispatch} className="flex flex-col gap-3">
         <div className="flex flex-col gap-2">
-          <FormInput type="text" placeholder="Username" required errors={[]} />
-          <FormInput type="email" placeholder="Email" required errors={[]} />
           <FormInput
+            name="username"
+            type="text"
+            placeholder="Username"
+            required
+            errors={state?.fieldErrors.username}
+          />
+          <FormInput
+            name="email"
+            type="email"
+            placeholder="Email"
+            required
+            errors={state?.fieldErrors.email}
+          />
+          <FormInput
+            name="password"
             type="password"
             placeholder="Password"
             required
-            errors={[]}
+            errors={state?.fieldErrors.password}
           />
           <FormInput
+            name="confirm_password"
             type="password"
             placeholder="Confirm Password"
             required
-            errors={[]}
+            errors={state?.fieldErrors.confirm_password}
           />
         </div>
-        <FormButton loading={false} text="Create account" />
+        <FormButton text="Create account" />
       </form>
       <SocialLogin />
     </div>
